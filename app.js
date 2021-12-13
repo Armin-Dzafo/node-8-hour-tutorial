dfb2d1e89a931d8b9561f77646435fa936d6e1c8
alojsb <alojsbirmingham@gmail.com>
2021-12-13 21:34:42 +0100

    blocking and async examples

--- app.js
+++ app.js
@@ -11,4 +11,19 @@
 // package.json - manifest file (stores important info about project/package)
 // manual approach create package.json in the root, create properties etc.)
 // npm init (step by step, press enter to skip)
-// npm init -y (everything default) 
+// npm init -y (everything default)
+
+
+const { readFile, writeFile } = require('fs')
+
+console.log('started a first task')
+// CHECK FILE PATH!!!!
+readFile('./content/first.txt', 'utf8', (err, result) => {
+  if (err) {
+    console.log(err)
+    return
+  }
+  console.log(result)
+  console.log('completed first task')
+})
+console.log('starting next task')
